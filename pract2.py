4a8147df999a00f97aaa85b5df3b6f06b61c1f1a
anasanchov <98358329+anasanchov@users.noreply.github.com>
2024-12-17 19:04:06 +0100

    Update pract2.py

--- pract2.py
+++ pract2.py
@@ -2,6 +2,7 @@ import GUI
 import HAL
 import utm
 import cv2
+import time
 # Enter sequential code!
 
 boat_lat = 40 + 16/60 + 48.2/3600 
@@ -25,33 +26,75 @@ y_pos = HAL.get_position()[1]
 print("DISTANCIA X: ", dist_x)
 print("DISTANCIA Y: ",dist_y)
 
-    #act_pos = HAL.get_position() # sacar la posición hasta que esté en el rango  dentro de las x
 
-while dist_x != x_pos and dist_y != y_pos: #ajustar valores, no redondeados (poner un rango, pe estar entre dist_y -1 y dist_y +1 o así)
-    #x_pos, y_pos = HAL.get_position() #devuelve array [x,y,z]
-    #print(HAL.get_position())
-    #print(y_pos)
-    HAL.set_cmd_pos(dist_x, dist_y, height, angle)
-    x_pos = HAL.get_position()[0]
-    y_pos = HAL.get_position()[1]
-    print(x_pos)
-    print(y_pos)
+def move_to_location(target_x, target_y, current_height):
+    while not((target_x-1 < HAL.get_position()[0] < target_x+1) and 
+              (target_y-1 < HAL.get_position()[1] < target_y+1)):
 
-    #if (act_position[0] < dist_x)
+        GUI.showImage(HAL.get_frontal_image())
+        GUI.showLeftImage(HAL.get_ventral_image())
 
-    GUI.showImage(HAL.get_frontal_image())
-    GUI.showLeftImage(HAL.get_ventral_image())
+        HAL.set_cmd_pos(target_x, target_y, current_height, angle)
+        x_pos = HAL.get_position()[0]
+        y_pos = HAL.get_position()[1]
+        #print(x_pos)
+        #print(y_pos)
+
+
+ 
+def detect_victims(image):
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    return faces
+
+
+def rescure_victims():
+    HAL.takeoff(height)
+
+    move_to_location(dist_x, dist_y, height)
+
+    total_victims = 6
+    rescued_victims = 0
+
+    while rescued_victims < total_victims:
+        front_image = HAL.get_frontal_image()
+        left_image = HAL.get_ventral_image()
+        
+        GUI.showImage(front_image)
+        GUI.showLeftImage(left_image)
+
+        victims = detect_victims(front_image)
+
+        #for(x, y, w, h) in 
+        print(victims)
+
+        for(x, y, w, h) in victims:
+            cv2.rectangle(front_image,(x,y), (x+w, y+h), (0, 255, 0), 2)
+            rescued_victims += 1
+        
+        GUI.showImage(front_image)
+
+        time.sleep(1)
+    
+    move_to_location(0, 0, height)
+
+    HAL.land()
+
+
+rescure_victims()
 
 
 while True:
+    pass
     #GUI.showImage(HAL.get_frontal_image())
     #GUI.showLeftImage(HAL.get_ventral_image())
 
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
- 
-#1. Ir hasta zona de rescate
+
+
+#1. Ir hasta zona de rescate 
 #2. Rescatar a los naufragos:
 #    - Load * xml              |
 #    - Movemos drone           |       cv2
@@ -61,5 +104,3 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 #    - 5 víctimas --> loop
 
 #3. Volver al barco '''
-
-
